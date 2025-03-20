cd407971a3c5824f4aed73e2829561b3ee53b923
sohumdalal <dsohum@gmail.com>
2025-03-20 15:26:19 -0400

    updated materials

--- webDev3/script.js
+++ webDev3/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (e) => {
+// document.addEventListener("DOMContentLoaded", (e) => {
 
 //Example 1: Simple console Logs
 // console.log(" I am now running JS in my browser")
@@ -6,7 +6,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
 // parent.textContent = "hello!"
 
 
-//Example 2: Dom element Creation
+
+
+
+
+
+//Example 2: DOM element Creation
 // const domDiv = document.createElement("div")
 // const domDivContent = document.createTextNode("Hello!")
 // domDiv.appendChild(domDivContent)
@@ -14,17 +19,44 @@ document.addEventListener("DOMContentLoaded", (e) => {
 // domDiv.id = "domDiv"
 // domDiv.style.color = "red"
 
+
+
+
+
+
+
+
 //Example 3
 // const children = document.querySelectorAll(".child");
 
-//     children.forEach(child => {
-//         child.addEventListener("click", () => {
-//             if (child.style.backgroundColor === "lightgray") {
-//                 child.style.backgroundColor = "lightblue";
-//             } else {
-//                 child.style.backgroundColor = "lightgray";
-//             }
-//         });
+// children.forEach(child => {
+//     child.addEventListener("click", () => {
+//         if (child.style.backgroundColor === "lightgray") {
+//             child.style.backgroundColor = "lightblue";
+//         } else {
+//             child.style.backgroundColor = "lightgray";
+//         }
 //     });
+// });
+
+
+
+// })
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    const parent = document.getElementById("parent")
+    // parent.textContent = "hello!"
+
+    const domDiv = document.createElement("div")
+    const domDivContent = document.createTextNode("Hello!")
+    domDiv.appendChild(domDivContent)
+    document.body.insertBefore(domDiv, parent)
+    domDiv.id = "domDiv"
+    domDiv.style.color = "red"
+
+
+
+
 
 })
