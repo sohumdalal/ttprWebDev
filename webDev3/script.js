3985e7610facb7d38eb5c9790f6c6d7b06e45ce6
sohumdalal <dsohum@gmail.com>
2025-03-20 11:52:44 -0400

    working tutorial

---
+++ webDev3/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+
+//Example 1: Simple console Logs
+// console.log(" I am now running JS in my browser")
+// const parent = document.getElementById("parent")
+// parent.textContent = "hello!"
+
+
+//Example 2: Dom element Creation
+// const domDiv = document.createElement("div")
+// const domDivContent = document.createTextNode("Hello!")
+// domDiv.appendChild(domDivContent)
+// document.body.insertBefore(domDiv, parent)
+// domDiv.id = "domDiv"
+// domDiv.style.color = "red"
+
+//Example 3
+// const children = document.querySelectorAll(".child");
+
+//     children.forEach(child => {
+//         child.addEventListener("click", () => {
+//             if (child.style.backgroundColor === "lightgray") {
+//                 child.style.backgroundColor = "lightblue";
+//             } else {
+//                 child.style.backgroundColor = "lightgray";
+//             }
+//         });
+//     });
+
+})
