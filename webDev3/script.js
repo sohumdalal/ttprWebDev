5c15abac7d96e20d475613037a0e32bd99bdea7f
sohumdalal <dsohum@gmail.com>
2025-03-20 20:20:30 -0400

    small adds

--- webDev3/script.js
+++ webDev3/script.js
@@ -1,4 +1,4 @@
-// document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
 
 //Example 1: Simple console Logs
 // console.log(" I am now running JS in my browser")
@@ -26,34 +26,34 @@
 
 
 
-//Example 3
-// const children = document.querySelectorAll(".child");
+// Example 3
+const children = document.querySelectorAll(".child");
 
-// children.forEach(child => {
-//     child.addEventListener("click", () => {
-//         if (child.style.backgroundColor === "lightgray") {
-//             child.style.backgroundColor = "lightblue";
-//         } else {
-//             child.style.backgroundColor = "lightgray";
-//         }
-//     });
-// });
+children.forEach(child => {
+    child.addEventListener("click", () => {
+        if (child.style.backgroundColor === "lightgray") {
+            child.style.backgroundColor = "lightblue";
+        } else {
+            child.style.backgroundColor = "lightgray";
+        }
+    });
+});
 
 
 
 // })
 
-document.addEventListener("DOMContentLoaded", (event) => {
+// document.addEventListener("DOMContentLoaded", (event) => {
 
-    const parent = document.getElementById("parent")
-    // parent.textContent = "hello!"
+//     const parent = document.getElementById("parent")
+//     // parent.textContent = "hello!"
 
-    const domDiv = document.createElement("div")
-    const domDivContent = document.createTextNode("Hello!")
-    domDiv.appendChild(domDivContent)
-    document.body.insertBefore(domDiv, parent)
-    domDiv.id = "domDiv"
-    domDiv.style.color = "red"
+//     const domDiv = document.createElement("div")
+//     const domDivContent = document.createTextNode("Hello!")
+//     domDiv.appendChild(domDivContent)
+//     document.body.insertBefore(domDiv, parent)
+//     domDiv.id = "domDiv"
+//     domDiv.style.color = "red"
 
 
 
