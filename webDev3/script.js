a80b9d9490797367e4e39efcb5f385215c45e056
sohumdalal <dsohum@gmail.com>
2025-03-25 15:27:11 -0400

    added examples of EDP + Async

--- webDev3/script.js
+++ webDev3/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     //Example 1: Simple console Logs
     // console.log(" I am now running JS in my browser")
-    const parent = document.getElementById("parent")
+    // const parent = document.getElementById("parent")
     // parent.textContent = "hello!"
 
 
@@ -43,22 +43,121 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 
 
-    // })
 
-    // document.addEventListener("DOMContentLoaded", (event) => {
 
-    //     const parent = document.getElementById("parent")
-    //     // parent.textContent = "hello!"
+    //Example 4: Button Clicked
+    // document.querySelector("button").addEventListener("click", (banana) => {
+    //     console.log("Button Clicked!");
+    //     console.log(banana);
 
-    //     const domDiv = document.createElement("div")
-    //     const domDivContent = document.createTextNode("Hello!")
-    //     domDiv.appendChild(domDivContent)
-    //     document.body.insertBefore(domDiv, parent)
-    //     domDiv.id = "domDiv"
-    //     domDiv.style.color = "red"
+    // });
 
 
 
+
+
+    //Example 5: Event Bubbling
+    // document.getElementById("parent").addEventListener("click", (e) => {
+    //     console.log("Parent Clicked");
+    // });
+
+    // document.querySelector(".child").addEventListener("click", (e) => {
+    //     console.log("Child Clicked");
+    //     e.stopPropagation(); // Stops bubbling!
+    // });
+
+
+
+
+
+
+    //Example 6: Event Delegation
+    // document.getElementById("parent").addEventListener("click", (event) => {
+    //     console.log("parent clicked, but not anyone buttons")
+    //     if (event.target.tagName === "BUTTON") {
+    //       console.log("Button Clicked:", event.target.textContent);
+    //     }
+    //   });
+
+
+
+
+
+    
+    // Example 7: Asynchroncity of Event Handling
+    document.querySelector(".child").addEventListener("click", () => {
+        console.log("Button Clicked!");
+
+        setTimeout(() => {
+            console.log("Now we are inside setTimeout (delayed execution)");
+        }, 0);
+
+        console.log("Event Listener Finished Executing");
+
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Solution to HARD 
+    // class EventEmitter {
+    //     constructor() {
+    //       this.events = {};
+    //     }
+      
+    //     on(event, callback) {
+    //       if (!this.events[event]) {
+    //         this.events[event] = [];
+    //       }
+    //       this.events[event].push(callback);
+    //     }
+      
+    //     emit(event, data) {
+    //       if (this.events[event]) {
+    //         this.events[event].forEach(callback => callback(data));
+    //       }
+    //     }
+    //   }
+      
+    //   const eventBus = new EventEmitter();
+      
+    //   eventBus.on("greet", (name) => console.log(`Hello, ${name}!`));
+      
+    //   eventBus.emit("greet", "Sohum");
 
 
 
